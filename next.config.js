e6f29b2c4662237590359375ae8c793918b977a6
zarifRahman <zarif.shahriar.rahman@gmail.com>
2023-11-03 21:22:50 +0600

    personal blog site

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 module.exports = nextConfig
